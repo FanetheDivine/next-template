b172dbce290f18ad2762146d50c39e61da2e9eff
FanetheDivine <1974269607@qq.com>
2025-11-02 02:29:55 +0800

    feat: 样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useEffect, useRef, type FC } from 'react'
+import { FC } from 'react'
 import { Button } from 'antd'
 import { AbsoluteCenter } from '@/styles'
 import { cn } from '@/utils'
@@ -8,8 +8,8 @@ import { sleep } from '@/utils/sleep'
 const Page: FC = async () => {
   await sleep(2000)
   return (
-    <Link href='/test1'>
-      <Button type='primary' className={cn(AbsoluteCenter, 'bg-black')}>
+    <Link href='/test1' className={AbsoluteCenter}>
+      <Button type='primary' className={'bg-black'}>
         Test Page
       </Button>
     </Link>
