69ddfc65d1cac942a1eb95c0999dc964a821a30f
FanetheDivine <1974269607@qq.com>
2025-11-02 13:50:44 +0000

    feat: config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+import { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 import nextAnalyzer from '@next/bundle-analyzer'
 
+const nextConfig: NextConfig = {}
+
+const withNextIntl = createNextIntlPlugin()
 const withAnalyzer = nextAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
-const nextConfig = withAnalyzer({})
 
-export default nextConfig
+export default withAnalyzer(withNextIntl(nextConfig))
