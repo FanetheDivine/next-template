e3c282c62841da962e5417a045dfbba9d778437b
FanetheDivine <1974269607@qq.com>
2025-11-02 14:57:48 +0000

    messages&formats

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,16 @@
-import { hasLocale } from 'next-intl'
+import { Formats, hasLocale } from 'next-intl'
 import { getRequestConfig } from 'next-intl/server'
 import { routing } from './routing'
+
+export const formats: Formats = {}
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale
   const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale
-
+  const { default: messages } = await import(`@/../messages/${locale}.json`)
   return {
     locale,
+    messages,
+    formats,
   }
 })
--- src/i18n/type.ts
+++ src/i18n/type.ts
@@ -1,9 +1,11 @@
 import locales from '@/../messages/zh.json'
-import { routing } from '@/i18n/routing'
+import { formats } from './request'
+import { routing } from './routing'
 
 declare module 'next-intl' {
   interface AppConfig {
     Locale: (typeof routing.locales)[number]
     Messages: typeof locales
+    Formats: typeof formats
   }
 }
