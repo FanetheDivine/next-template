587dcd244c071be2a392588bf365c0ad4eb51369
Fane <godwokenfane@gmail.com>
2025-11-03 02:49:55 +0000

    测试组件

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import { getLocale, getTranslations } from 'next-intl/server'
 import { FC } from 'react'
 import { Button } from 'antd'
 import { AbsoluteCenter } from '@/styles'
@@ -6,7 +7,15 @@ import { sleep } from '@/utils/sleep'
 
 const Page: FC = async () => {
   await sleep(2000)
-  return 1
+  const locale = await getLocale()
+  const t = await getTranslations()
+  return (
+    <Link href='/test1' className={AbsoluteCenter}>
+      <Button type='primary' className={'bg-black'}>
+        {[locale, t('metadata.title')]}
+      </Button>
+    </Link>
+  )
 }
 
 export default Page
--- src/app/[locale]/test1/page.tsx
+++ src/app/[locale]/test1/page.tsx
@@ -1,13 +1,14 @@
+import { getTranslations } from 'next-intl/server'
 import { FC } from 'react'
 import { Link } from '@/i18n/navigation'
 
-const Page: FC = () => {
-  return null
-  // return (
-  //   <Link href='/' locale='en'>
-  //     to home
-  //   </Link>
-  // )
+const Page: FC = async () => {
+  const t = await getTranslations()
+  return (
+    <Link href='/' locale='en'>
+      {t('common.toHomepage')}
+    </Link>
+  )
 }
 
 export default Page
