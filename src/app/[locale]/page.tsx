7b34634727edf39285671dfaeeb6d88838e45deb
FanetheDivine <1974269607@qq.com>
2025-11-04 02:42:36 +0000

    feat :

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import { getLocale, getTranslations } from 'next-intl/server'
 import { FC } from 'react'
 import { Button } from 'antd'
 import { AbsoluteCenter } from '@/styles'
+import { isMobile } from '@/utils'
 import { Link } from '@/i18n/navigation'
 import { sleep } from '@/utils/sleep'
 import { ConfirmBtn } from './client-components/ConfirmBtn'
@@ -10,9 +11,12 @@ const Page: FC = async () => {
   await sleep(2000)
   const locale = await getLocale()
   const t = await getTranslations()
+  const ua = await isMobile()
+  console.log(ua)
   return (
     <>
       <ConfirmBtn />
+      {JSON.stringify(ua)}
       <Link href='/test1' className={AbsoluteCenter}>
         <Button type='primary' className={'bg-black'}>
           {[locale, t('metadata.title')]}
