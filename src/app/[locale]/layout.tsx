c1003d29c46d900c5259aa3d74c8db70fa4122f5
Fane <godwokenfane@gmail.com>
2025-12-02 09:40:58 +0000

    PageProps

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,23 +7,24 @@ import { FC, PropsWithChildren } from 'react'
 import { App } from 'antd'
 import { match } from 'ts-pattern'
 import { routing } from '@/i18n/routing'
-import { type LocaleParams } from '@/i18n/type'
 import AntdProvider from '@/lib/AntdProvider'
 import SWRProvider from '@/lib/SWRProvider'
 
-export async function generateMetadata(props: LocaleParams): Promise<Metadata> {
+export async function generateMetadata(props: PageProps<'/[locale]'>): Promise<Metadata> {
   // 为了适应打包为静态html的情况
   const { params } = props
   const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    return {}
+  }
   const t = await getTranslations({ locale })
   return {
     title: t('metadata.title'),
   }
 }
 
-const RootLayout: FC<PropsWithChildren> = async (props) => {
-  // 不能放在FC的类型里面 过不了检查
-  const { children, params } = props as PropsWithChildren & LocaleParams
+const RootLayout: FC<PropsWithChildren & PageProps<'/[locale]'>> = async (props) => {
+  const { children, params } = props
 
   const locale = await match(process.env.EXPORT === 'true')
     .with(true, async () => {
